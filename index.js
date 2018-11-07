5725b6980609cd5acb17c3a0a75daa1e7c888b6b
Aivoapina <anssi.varjonen@gmail.com>
2018-11-07 23:09:04 +0200

    Added horaro schedule support

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ var app = require('express')()
 var wsapp = require('express-ws')(app)
 var hashids = require('hashids')
 var hasher = new hashids('salt done drunk')
+var schedule = require('./schedule.js'); // schedule from horaro
+var path = require('path');
+
 var odd = {}
 
 app.ws('*', (ws, req) => {
@@ -108,6 +111,10 @@ app.get('/', (req, res, next) => {
 	res.redirect(hasher.encode(+new Date))
 })
 
+app.get('/schedule', (req, res, next) => {
+	res.json(schedule);
+})
+
 app.get('*', (req, res, next) => {
 	var id = req.params[0].split('/')[1].toString()
 	if(id) {
@@ -117,100 +124,6 @@ app.get('*', (req, res, next) => {
 			odd[id] = self
 		}
 	}
-	res.send(`<!doctype html>
-<html>
-<head>
-<meta charset='utf-8'>
-<meta name='viewport' content='width=device-width, initial-scale=1'>
-<title>Overlay Done Drunk</title>
-<style>
-* { margin: 0; padding: 0; background: transparent; }
-body { font-family: sans-serif; }
-#timer { font-size: 60px; position: absolute; bottom: 0; right: 0; padding: 0 10px 0 0; font-family: 'Calibri', sans-serif; font-weight: bold; }
-#timer span { font-size: 0.7em; }
-#controls { width: 200px; height: 200px; position: absolute; top: 50%; left: 50%; margin: -100px -100px; }
-#controls button { width: 100%; padding: 10px; text-transform: uppercase; }
-.text { white-space: pre; padding: 10px; font-size: 20px; position: absolute; overflow: visible; box-sizing: border-box; width: 33.333% }
-#t { top: 0; left: 10%; width: 80%; text-align: center; }
-#tl { top: 0; left: 0; }
-#tr { top: 0; right: 0; text-align: right; }
-#b { bottom: 0; left: 10%; width: 80%; text-align: center; }
-#bl { bottom: 0; left: 0; }
-#br { bottom: 0; right: 0; text-align: right; }
-</style>
-</head>
-<body>
-<div id='container'>
-<div class='reset' id='timer'></div>
-<div class='text' id='tl' contenteditable='true'></div>
-<div class='text' id='t' contenteditable='true'></div>
-<div class='text' id='tr' contenteditable='true'></div>
-<div class='text' id='bl' contenteditable='true'></div>
-<div class='text' id='b' contenteditable='true'></div>
-<div class='text' id='br' contenteditable='true'></div>
-<div id='controls'><button>start</button><button>pause</button><button>stop</button><button>reset</button></div>
-</div>
-<script src='//cdnjs.cloudflare.com/ajax/libs/reconnecting-websocket/1.0.0/reconnecting-websocket.min.js'></script>
-<script>
-"use strict";
-var odd = { started: 0, paused: 0, stopped: 0, error: 0, text: {} },
-	timer = document.getElementById('timer'),
-	controls = document.getElementById('controls')
-var ttime = function(time) {
-  time = Math.abs(time)
-  if(time == 0) return ''
-  var s = (time / 1000) % 60
-  var m = Math.floor((time / (1000 * 60)) % 60)
-  if(m < 10) m = '0'+m
-  var h = Math.floor((time / (1000 * 60 * 60)) % 24)
-  if(h < 10) h = '0'+h
-  var newTime = h+':'+m+':'+(s<10?'0':'')+s.toFixed(2)
-  newTime = newTime.replace(/^[0:]+/, '')
-  newTime = newTime.replace(/^[\.]/, '0.')
-  newTime = newTime.replace('.','<span>.')+'</span>'
-  return newTime
-}
-var format = function() {
-	if(odd.stopped) return ttime(odd.stopped-odd.started)
-	else if(odd.paused) return ttime(odd.paused-odd.started)
-	else if(odd.started) return ttime(+new Date()-odd.started+odd.error)
-	else return ttime(0)
-}
-var ws = new ReconnectingWebSocket(window.location.href.replace(/(^http)/, 'ws'))
-ws.onopen = function(e) {
-	ws.onmessage = function(e) {
-		odd = JSON.parse(e.data)
-		console.log(odd)
-		if(!odd.error) odd.error = odd.now - +new Date
-		for(var i of Object.keys(odd.text)) {
-			document.getElementById(i).innerHTML = odd.text[i]
-		}
-		if(odd.stopped) timer.className = 'stop'
-		else if(odd.paused) timer.className = 'pause'
-		else if(odd.started) timer.className = 'start'
-	}
-	ws.send(JSON.stringify({ cmd: 'status' }))
-}
-setInterval(function() {
-	timer.innerHTML = format()
-}, 10)
-for(var i of document.getElementsByTagName('button')) {
-	i.addEventListener('click', (e) => {
-		var cmd = { cmd: e.target.innerHTML, arg: '' }
-		ws.send(JSON.stringify(cmd))
-	}, false)
-}
-for(var i of document.getElementsByClassName('text')) {
-	i.addEventListener('blur', (e) => {
-		if(e.target.textContent == '') e.target.innerHTML = ''
-		var cmd = { cmd: 'text', arg: { pos: e.target.id, text: e.target.innerHTML }}
-		ws.send(JSON.stringify(cmd))
-	}, false)
-}
-</script>
-</body>
-</html>
-`)
-	res.end()
+	res.sendFile(path.join(__dirname + '/index.html'));
 })
 app.listen(process.env.ODD_PORT || 4209)
